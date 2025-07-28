670631c1314002631686bdb3f9862c74a180afd9
Watila Costa <watilafreire26@gmail.com>
2025-07-27 22:20:25 -0300

    adicionando lucid icon / ajustando styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,41 @@ import './styles/global.css'
 import './styles/theme.css'
 
 import { Heading } from './components/Heading'
+import { TimerIcon } from 'lucide-react'
 
 function App() {
   return (
     <>
-      <Heading />
+      {/* <Heading>
+        Chronos Pomodoro
+        <button>
+          <TimerIcon color='#0a0f1a' />
+        </button>
+      </Heading> */}
+
+      <div className='container'>
+        <div className='content'>
+          <section>logo</section>
+        </div>
+      </div>
+
+      <div className='container'>
+        <div className='content'>
+          <section>menu</section>
+        </div>
+      </div>
+
+      <div className='container'>
+        <div className='content'>
+          <section>form</section>
+        </div>
+      </div>
+
+      <div className='container'>
+        <div className='content'>
+          <section>footer</section>
+        </div>
+      </div>
     </>
   )
 }
--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,5 +1,11 @@
 import styles from './Heading.module.css'
 
-export function Heading() {
-  return <h1 className={styles.heading}>Chronos Pomodoro</h1>
+type HeadingProps = {
+  children: React.ReactNode /* tudo que o react pode renderizar */
+}
+
+export function Heading(props: HeadingProps) {
+  const { children } = props /* destructuring */
+
+  return <h1 className={styles.heading}>{children}</h1>
 }
